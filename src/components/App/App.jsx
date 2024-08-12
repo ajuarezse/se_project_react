fc88dce6232493aaec05780037c2693073bcba66
ajuarezse <ajuarez.se@gmail.com>
2024-08-12 09:31:26 -0700

    add modal open and close function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,12 +8,18 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
-  const [activeModal, setActiveModal] = useState("t");
+  const [activeModal, setActiveModal] = useState("");
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
 
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
         <Footer />
       </div>
@@ -21,6 +27,7 @@ function App() {
         title="New garment"
         buttonText="Add garment"
         activeModal={activeModal}
+        handleCloseClick={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label modal__label_text">
           Name{""}
