9cecfe0939137898171c0751bbfdccaa08c9f4d8
ajuarezse <ajuarez.se@gmail.com>
2024-08-11 20:38:02 -0700

    construct and stylize footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
       <div className="page__content">
         <Header />
         <Main weatherData={weatherData} />
+        <Footer />
       </div>
-      <ModalWithForm />
+      {/* <ModalWithForm /> */}
     </div>
   );
 }
