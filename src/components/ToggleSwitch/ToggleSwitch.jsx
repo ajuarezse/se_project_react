91ae2cdbaa4d21f735701e00446563e76ee66be1
ajuarezse <ajuarez.se@gmail.com>
2024-08-21 19:05:45 -0700

    initiate toggle markup

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ToggleSwitch = () => {
+  console.log("toggle");
+  return (
+    <label>
+      <input type="checkbox" />
+    </label>
+  );
+};
+
+export default ToggleSwitch;
