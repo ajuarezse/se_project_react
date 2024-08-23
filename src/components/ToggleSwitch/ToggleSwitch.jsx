e633669487657005ef9e3ddb602a81e9f2a3001a
ajuarezse <ajuarez.se@gmail.com>
2024-08-23 11:09:38 -0700

    add slider to toggle

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
-  console.log("toggle");
+  const [currentTemperatureUnit, handleToggleSwitchChange] = useState("C");
+
+  const handleChange = (e) => {
+    if (currentTemperatureUnit === "C") handleToggleSwitchChange("F");
+    if (currentTemperatureUnit === "F") handleToggleSwitchChange("C");
+  };
+  console.log(currentTemperatureUnit);
   return (
     <label className="switch">
-      <input type="checkbox" className="switch__box" />
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+      <p>F</p>
+      <p>C</p>
     </label>
   );
 };
