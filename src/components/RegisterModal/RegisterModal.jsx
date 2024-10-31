3a11f810dd1d832e69dd926832e3d7b3cf1b34b7
ajuarezse <ajuarez.se@gmail.com>
2024-10-31 15:49:36 -0700

    add register inputs

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,9 +1,35 @@
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useState, useEffect } from "react";
 
-const RegisterModal = ({}) = {
-return (
-    
-)
+const RegisterModal = ({ isOpen, onClose }) => {
+  const [userEmail, setUserEmail] = useState("");
+
+  return (
+    <ModalWithForm
+      title="Sign Up"
+      buttonText="Sign Up"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={onSubmit}
+    >
+      <label htmlFor="email">
+        Email*
+        <input
+          type="email"
+          name="email"
+          className="modal__input"
+          placeholder="Email"
+          value={userEmail}
+        />
+      </label>
+      <label htmlFor="password">
+        Password*
+        <input type="password" />
+      </label>
+      <label htmlFor="name">Name*</label>
+      <label htmlFor="avatar">Avatar URL*</label>
+    </ModalWithForm>
+  );
 };
 
 export default RegisterModal;
