246f177a20cb22d81ed1f4d3335b4f760fbb60f1
ajuarezse <ajuarez.se@gmail.com>
2024-11-01 13:35:36 -0700

    add password logic

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -2,13 +2,20 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import { useState, useEffect } from "react";
 
 const LoginModal = ({ isOpen, onClose, onLogin }) => {
-
-const [userEmail, setUserEmail] = useState("");
-const [userPassword, setUserPassword] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
   const user = {
     email: userEmail,
     password: userPassword,
+  };
+
+  const handleEmailChange = (e) => {
+    setUserEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setUserPassword(e.target.vaule);
   };
 
   const handleSubmit = (e) => {
@@ -24,15 +31,30 @@ const [userPassword, setUserPassword] = useState("");
       onClose={onClose}
       onSubmit={handleSubmit}
     >
-      <label htmlFor="email">Email<input 
-      type="email"
-      name="email"
-      id="email"
-      className="modal__input"
-      placeholder="Email"
-      value={userEmail}
-      onChange={}
-      /></label>
+      <label htmlFor="email">
+        Email
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="modal__input"
+          placeholder="Email"
+          value={userEmail}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="modal__input"
+          placeholder="Password"
+          value={userPassword}
+          onChange={handlePasswordChange}
+        />
+      </label>
     </ModalWithForm>
   );
 };
