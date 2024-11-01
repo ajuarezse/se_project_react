e9668fcc1a408170264a10b6345731998cec58d7
ajuarezse <ajuarez.se@gmail.com>
2024-11-01 12:35:45 -0700

    configure login modal

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,3 +1,40 @@
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import { useState, useEffect } from "react";
 
-const LoginModal = () => {};
+const LoginModal = ({ isOpen, onClose, onLogin }) => {
+
+const [userEmail, setUserEmail] = useState("");
+const [userPassword, setUserPassword] = useState("");
+
+  const user = {
+    email: userEmail,
+    password: userPassword,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(user);
+  };
+
+  return (
+    <ModalWithForm
+      title="Log In"
+      buttonText="Log In"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="email">Email<input 
+      type="email"
+      name="email"
+      id="email"
+      className="modal__input"
+      placeholder="Email"
+      value={userEmail}
+      onChange={}
+      /></label>
+    </ModalWithForm>
+  );
+};
+
+export default LoginModal;
