46c14b863178a4cd33d187ef019e89540a873ac8
ajuarezse <ajuarez.se@gmail.com>
2024-11-04 14:08:35 -0800

    configure signup signin

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,27 @@
-export const signup = () => {
-    return fetch(`${}/signup`, {
-        
-    })
-}
+const baseUrl = "http://localhost:3001";
 
-export const signin = () => {};
+export const signup = (name, avatar, password, email) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, password, email }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+export const signin = (email, password) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
