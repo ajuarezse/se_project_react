8de61706891ce967c63563f363bc86ec25039cc5
ajuarezse <ajuarez.se@gmail.com>
2024-11-08 16:23:57 -0800

    add sidebar buttons

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,14 +6,20 @@ import CurrentUserContext from "../../contexts/CurrentUserContext/CurrentUserCon
 function SideBar() {
   const currentUser = useContext(CurrentUserContext);
   return (
-    <div className="sidebar">
-      <img
-        className="sidebar__avatar"
-        src={currentUser.avatar}
-        alt="default avatar"
-      />
-      <p className="sidebar__username">{currentUser?.name}</p>
-    </div>
+    <>
+      <div className="sidebar">
+        <img
+          className="sidebar__avatar"
+          src={currentUser.avatar}
+          alt="default avatar"
+        />
+        <p className="sidebar__username">{currentUser?.name}</p>
+      </div>
+      <div className="sidebar__directory">
+        <button className="sidebar__button">Change profile data</button>
+        <button className="sidebar__button">Log out</button>
+      </div>
+    </>
   );
 }
 
