3f32a42ea93c12c558e2bd2d69859bcfbc091afd
ajuarezse <ajuarez.se@gmail.com>
2024-12-21 16:48:37 -0800

    add name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <p className="footer__credit">Developed by A. Juarez</p>
+        <p className="footer__credit">Developed by Angel Juarez</p>
         <p className="footer__year">2025</p>
       </div>
     </footer>
