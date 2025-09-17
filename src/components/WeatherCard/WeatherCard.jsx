e6b8b787d19581979ed61fe868919b4250a73e44
ajuarezse <ajuarez.se@gmail.com>
2025-09-17 16:13:05 -0700

    Feature: Add day/night weather images - Add nighttime weather images - Implement time-based image switching - Update tests to cover day/night scenarios

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
+// Day images
 import sunny from "../../assets/sunny.png";
 import cloudy from "../../assets/cloudy.png";
 import fog from "../../assets/fog.png";
 import rain from "../../assets/rain.png";
 import snow from "../../assets/snow.png";
 import storm from "../../assets/storm.png";
+// Night images
+import sunnyDark from "../../assets/sunnyDark.png";
+import cloudyDark from "../../assets/cloudyDark.png";
+import fogDark from "../../assets/fogDark.png";
+import rainDark from "../../assets/rainDark.png";
+import snowDark from "../../assets/snowDark.png";
+import stormDark from "../../assets/stormDark.png";
+
 import "./WeatherCard.css";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
-const weatherImages = {
+const dayWeatherImages = {
   clear: sunny,
   clouds: cloudy,
   fog: fog,
@@ -19,13 +28,30 @@ const weatherImages = {
   default: sunny,
 };
 
+const nightWeatherImages = {
+  clear: sunnyDark,
+  clouds: cloudyDark,
+  fog: fogDark,
+  mist: fogDark,
+  rain: rainDark,
+  snow: snowDark,
+  thunderstorm: stormDark,
+  default: sunnyDark,
+};
+
 function WeatherCard({ weatherData }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   const temperature =
     currentTemperatureUnit === "F" ? weatherData.temp.F : weatherData.temp.C;
 
+  const isNighttime = () => {
+    const hour = new Date().getHours();
+    return hour < 6 || hour >= 18; // Night time between 6 PM and 6 AM
+  };
+
   const getWeatherImage = (condition) => {
-    return weatherImages[condition] || weatherImages.default;
+    const images = isNighttime() ? nightWeatherImages : dayWeatherImages;
+    return images[condition] || images.default;
   };
 
   return (
@@ -35,7 +61,7 @@ function WeatherCard({ weatherData }) {
       </p>
       <img
         src={getWeatherImage(weatherData.condition)}
-        alt={weatherData.condition}
+        alt={`${weatherData.condition} ${isNighttime() ? 'night' : 'day'}`}
         className="weather-card__image"
       />
     </section>
